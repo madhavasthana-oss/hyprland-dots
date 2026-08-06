baf5eb7b7f871e43dfd1505e543a2f6a7b130386
madhavasthana-oss <madhavasthana@gmail.com>
2026-08-06 09:02:33 +0530

    removed zsh folder

--- .config/zsh/prompt.zsh
+++
@@ -1,3 +0,0 @@
-# Monoshell oh-my-posh prompt (Ash)
-# Themes: ash-info-util | ash-pill | ash-ribbon under ~/.config/oh-my-posh/
-eval "$(oh-my-posh init zsh --config ${XDG_CONFIG_HOME:-$HOME/.config}/oh-my-posh/ash-info-util.omp.json)"
